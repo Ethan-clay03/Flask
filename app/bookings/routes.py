01e07d190eecc7642638fee34b807e5b2c32efc1
Ethan-clay03 <ethanclay2017@gmail.com>
2025-01-15 12:46:48 +0000

    Fix bookings page and simplify logic by moving into flask route

--- app/bookings/routes.py
+++ app/bookings/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, g
 from app.bookings import bp
 from app.models import Listings, ListingImages 
+import json
 
 @bp.route('/home')
 def index():
@@ -18,11 +19,24 @@ def index():
 def redirect_index():
     return redirect(url_for('bookings.index'), code=301)
 
+
 @bp.route('/listings')
 def listings():
     all_listings = Listings.get_all_listings()
-    
+
+    for item in all_listings:
+        main_image = next((img for img in item.listing_images if img.main_image), None)
+        if main_image:
+            item.main_image_url = url_for('main.upload_file', filename=main_image.image_location)
+        elif item.listing_images:
+            item.main_image_url = url_for('main.upload_file', filename=item.listing_images[0].image_location)
+        else:
+            item.main_image_url = "/path/to/default-image.jpg"
+
+        item.image_urls = json.dumps([url_for('main.upload_file', filename=img.image_location) for img in item.listing_images])
+
     return render_template('bookings/listings.html', items=all_listings)
+
 
 @bp.route('/listing/<int:id>')
 def show_listing(id):
